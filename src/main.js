2a04147873019329a6cd1de1cebc9bbfa2da736e
微笑向前 <805558395@qq.com>
2022-08-01 20:18:23 +0800

    提交添加vant组件库

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
 // 全局样式
 import './styles/index.less'
+// 使用组件
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
